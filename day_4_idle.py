7d99429203f25007d5248fc3c78536d1a692dd11
Amit Chawarekar <54398300+Amitchawarekar@users.noreply.github.com>
2021-07-17 16:12:30 +0530

    Add files via upload

---
+++ day_4_idle.py
@@ -0,0 +1,17 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=eval(input('Enter your Age:'))
+Enter your Age:29
+>>> a
+29
+>>> type(a)
+<class 'int'>
+>>> b=eval(input('Enter your temp:'))
+Enter your temp:32.4
+>>> b
+32.4
+>>> type(b)
+<class 'float'>
+>>> print('Even')if not a%2 else print('odd')
+odd
+>>> 
